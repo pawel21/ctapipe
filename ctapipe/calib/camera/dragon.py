4e0b4e43427fa02dd9b816ba11df7ae5378390c0
Paweł Gliwny <p.gliwny@gmail.com>
2018-07-14 13:12:45 +0200

    fix error in read first capacitor

--- ctapipe/calib/camera/dragon.py
+++ ctapipe/calib/camera/dragon.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from ctapipe.core import Component, Factory, traits
@@ -29,17 +30,27 @@ class DragonPedestal:
 
     def fill_pedestal_event(self, event, nr):
         first_cap = event.lst.tel[0].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
-        for channel_hg in [0, 2, 4, 6]:
-            self.first_capacitor[self.high_gain, channel_hg] = first_cap[channel_hg]
-            self.first_capacitor[self.high_gain, channel_hg + 1] = first_cap[channel_hg]
-        for channel_lg in [1, 3, 5, 7]:
-            self.first_capacitor[self.low_gain, channel_lg - 1] = first_cap[channel_lg]
-            self.first_capacitor[self.low_gain, channel_lg] = first_cap[channel_lg]
+        self.first_capacitor[self.high_gain, 0] = first_cap[0]
+        self.first_capacitor[self.high_gain, 1] = first_cap[0]
+        self.first_capacitor[self.high_gain, 2] = first_cap[1]
+        self.first_capacitor[self.high_gain, 3] = first_cap[1]
+        self.first_capacitor[self.high_gain, 4] = first_cap[2]
+        self.first_capacitor[self.high_gain, 5] = first_cap[2]
+        self.first_capacitor[self.high_gain, 6] = first_cap[3]
+
+        self.first_capacitor[self.low_gain, 0] = first_cap[4]
+        self.first_capacitor[self.low_gain, 1] = first_cap[4]
+        self.first_capacitor[self.low_gain, 2] = first_cap[5]
+        self.first_capacitor[self.low_gain, 3] = first_cap[5]
+        self.first_capacitor[self.low_gain, 4] = first_cap[6]
+        self.first_capacitor[self.low_gain, 5] = first_cap[6]
+        self.first_capacitor[self.low_gain, 6] = first_cap[7]
+
         waveform = event.r0.tel[0].waveform[:, nr * 7:(nr + 1) * 7, :]
         for i in range(0, 2):
             for j in range(0, self.n_pixels):
                 fc = int(self.first_capacitor[i, j])
-                for k in range(0, self.roisize):
+                for k in range(2, self.roisize-2):
                     posads = int((k+fc)%self.size4drs)
                     val = waveform[i, j, k]
                     self.meanped[i, j, posads] += val
@@ -60,12 +71,22 @@ def get_first_capacitor(event, nr):
     lg = 1
     fc = np.zeros((2, 8))
     first_cap = event.lst.tel[0].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
-    for channel_hg in [0, 2, 4, 6]:
-        fc[hg, channel_hg] = first_cap[channel_hg]
-        fc[hg, channel_hg + 1] = first_cap[channel_hg]
-    for channel_lg in [1, 3, 5, 7]:
-        fc[lg, channel_lg - 1] = first_cap[channel_lg]
-        fc[lg, channel_lg] = first_cap[channel_lg]
+
+    fc[hg, 0] = first_cap[0]
+    fc[hg, 1] = first_cap[0]
+    fc[hg, 2] = first_cap[1]
+    fc[hg, 3] = first_cap[1]
+    fc[hg, 4] = first_cap[2]
+    fc[hg, 5] = first_cap[2]
+    fc[hg, 6] = first_cap[3]
+
+    fc[lg, 0] = first_cap[4]
+    fc[lg, 1] = first_cap[4]
+    fc[lg, 2] = first_cap[5]
+    fc[lg, 3] = first_cap[5]
+    fc[lg, 4] = first_cap[6]
+    fc[lg, 5] = first_cap[6]
+    fc[lg, 6] = first_cap[7]
     return fc
 
 
@@ -93,3 +114,22 @@ def remove_pedestal(event, pedestal, nr):
                 val = event.r0.tel[0].waveform[i, nr * 7:(nr + 1) * 7, k][j] - int(
                 pedestal.meanped[i, j, position]) + offset
                 event.r0.tel[0].waveform[i, nr * 7:(nr + 1) * 7, k][j] = val
+
+
+def remove_pedestal_and_plot(ev, ped, nr, gain, pixel):
+    t = np.linspace(2, 38, 36)
+
+    fig, ax = plt.subplots()
+    ax.step(t, ev.r0.tel[0].waveform[gain, nr * 7:(nr + 1) * 7, 2:38][pixel], color="blue",
+            label="before remove pedestal", lw=3)
+    remove_pedestal(ev, ped, nr)
+    ax.step(t, ev.r0.tel[0].waveform[gain, nr * 7:(nr + 1) * 7, 2:38][pixel], color="red",
+            label="after remove pedestal", lw=3, alpha=0.75)
+
+    ax.plot([2, 38], [300, 300], 'k--', label="offset", lw=2)
+    ax.set_xlabel("time sample [ns]")
+    ax.set_ylabel("signal [counts]")
+    ax.set_ylim([150, 400])
+    ax.grid()
+    ax.legend()
+    plt.show()
