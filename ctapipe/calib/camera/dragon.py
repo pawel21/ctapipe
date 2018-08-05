d9c7b18e3b1aeb0b66d67bebdaa28c78e580fd28
Paweł Gliwny <p.gliwny@gmail.com>
2018-08-05 22:47:33 +0200

    create bin file to remove pedestal for all modules

--- ctapipe/calib/camera/dragon.py
+++ ctapipe/calib/camera/dragon.py
@@ -3,16 +3,7 @@ import numpy as np
 
 from ctapipe.core import Component, Factory, traits
 
-__all__ = ["Dragon", "DragonPedestal"]
-
-
-class Dragon():
-    """
-    Base class fot calibration dragon
-    """
-
-    def __init__(self):
-        pass
+__all__ = ["DragonPedestal", "DRS4Pedestal", "remove_pedestal_from_file"]
 
 
 class DragonPedestal:
@@ -30,21 +21,12 @@ class DragonPedestal:
 
     def fill_pedestal_event(self, event, nr):
         first_cap = event.lst.tel[0].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
-        self.first_capacitor[self.high_gain, 0] = first_cap[0]
-        self.first_capacitor[self.high_gain, 1] = first_cap[0]
-        self.first_capacitor[self.high_gain, 2] = first_cap[1]
-        self.first_capacitor[self.high_gain, 3] = first_cap[1]
-        self.first_capacitor[self.high_gain, 4] = first_cap[2]
-        self.first_capacitor[self.high_gain, 5] = first_cap[2]
-        self.first_capacitor[self.high_gain, 6] = first_cap[3]
 
-        self.first_capacitor[self.low_gain, 0] = first_cap[4]
-        self.first_capacitor[self.low_gain, 1] = first_cap[4]
-        self.first_capacitor[self.low_gain, 2] = first_cap[5]
-        self.first_capacitor[self.low_gain, 3] = first_cap[5]
-        self.first_capacitor[self.low_gain, 4] = first_cap[6]
-        self.first_capacitor[self.low_gain, 5] = first_cap[6]
-        self.first_capacitor[self.low_gain, 6] = first_cap[7]
+        for i, j in zip([0, 1, 2, 3, 4, 5, 6], [0, 0, 1, 1, 2, 2, 3]):
+            self.first_capacitor[self.high_gain, i] = first_cap[j]
+
+        for i, j in zip([0, 1, 2, 3, 4, 5, 6], [4, 4, 5, 5, 6, 6, 7]):
+            self.first_capacitor[self.low_gain, i] = first_cap[j]
 
         waveform = event.r0.tel[0].waveform[:, nr * 7:(nr + 1) * 7, :]
         for i in range(0, 2):
@@ -71,26 +53,13 @@ def get_first_capacitor(event, nr):
     lg = 1
     fc = np.zeros((2, 8))
     first_cap = event.lst.tel[0].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
-
-    fc[hg, 0] = first_cap[0]
-    fc[hg, 1] = first_cap[0]
-    fc[hg, 2] = first_cap[1]
-    fc[hg, 3] = first_cap[1]
-    fc[hg, 4] = first_cap[2]
-    fc[hg, 5] = first_cap[2]
-    fc[hg, 6] = first_cap[3]
-
-    fc[lg, 0] = first_cap[4]
-    fc[lg, 1] = first_cap[4]
-    fc[lg, 2] = first_cap[5]
-    fc[lg, 3] = first_cap[5]
-    fc[lg, 4] = first_cap[6]
-    fc[lg, 5] = first_cap[6]
-    fc[lg, 6] = first_cap[7]
+    for i, j in zip([0, 1, 2, 3, 4, 5, 6], [0, 0, 1, 1, 2, 2, 3]):
+        fc[hg, i] = first_cap[j]
+    for i, j in zip([0, 1, 2, 3, 4, 5, 6], [4, 4, 5, 5, 6, 6, 7]):
+        fc[lg, i] = first_cap[j]
     return fc
 
 
-# nr - number module
 def remove_pedestal(event, pedestal, nr):
     """
 
@@ -98,6 +67,10 @@ def remove_pedestal(event, pedestal, nr):
     ----------
     event: container
         A `ctapipe` event container
+    pedestal:
+        pedestal object
+    nr: int
+        number of module
     """
     first_cap = get_first_capacitor(event, nr)
     hg = 0
@@ -133,3 +106,44 @@ def remove_pedestal_and_plot(ev, ped, nr, gain, pixel):
     ax.grid()
     ax.legend()
     plt.show()
+
+
+class DRS4Pedestal():
+    def __init__(self, path_to_file, nr):
+        self.path = path_to_file
+        self.nr = nr
+        self.pedestal_value = np.zeros((self.nr, 2, 7, 4096))
+
+    def read_binary_file(self):
+        with open(self.path, "rb") as binary_file:
+            # Read the whole file at once
+            data = binary_file.read()
+
+            pos = 7
+            for i in range(0, self.nr):
+                for gain in range(0, 2):
+                    for pixel in range(0, 7):
+                        for cap in range(0, 4096):
+                            value = int.from_bytes(data[pos:pos + 2], byteorder='big')
+                            self.pedestal_value[i, gain, pixel, cap] = value
+                            pos += 2
+
+
+def remove_pedestal_from_file(event, pedestal):
+    waveform_after_remove_pedestal = np.zeros((event.r0.tel[0].waveform.shape))
+    n_pixels = 7
+    size4drs = 4 * 1024
+    roisize = 40
+    offset = 300
+    number_of_modules = event.lst.tel[0].svc.num_modules
+    for nr in range(0, number_of_modules):
+        first_cap = get_first_capacitor(event, nr)
+        for i in range(0, 2):
+            for j in range(0, n_pixels):
+                for k in range(0, roisize):
+                    position = int((k + first_cap[i, j]) % size4drs)
+                    val = (event.r0.tel[0].waveform[i, nr * 7:(nr + 1) * 7, k][j] - int(
+                        pedestal.pedestal_value[nr, i, j, position])) + offset
+                    waveform_after_remove_pedestal[i, nr * 7:(nr + 1) * 7, k][j] = val
+
+    return waveform_after_remove_pedestal
---
+++ examples/create_binary_file_with_pedestal.py
@@ -0,0 +1,60 @@
+import numpy as np
+import os
+import struct
+from ctapipe.io.lsteventsource import LSTEventSource
+from ctapipe.io import EventSeeker
+from ctapipe.calib.camera import DragonPedestal
+
+
+def write_pedestal_to_file(PedList, number_modules):
+    f_out = open('pedestal_value.dat', 'wb')
+    # header
+    f_out.write(struct.pack('>B', 1))  # version 1
+    f_out.write(struct.pack('>H', 7))  # number of pixels
+    f_out.write(struct.pack('>H', 4096))  # number of samples
+    f_out.write(struct.pack('>H', 40))  # RoI
+
+    for nr in range(0, number_modules):
+        # high gain
+        for pixel in range(0, 7):
+            pedestal_value = (PedList[nr].meanped[0, pixel, :])
+            for value in (pedestal_value):
+                if np.isnan(value):
+                    value = 0
+                    f_out.write(struct.pack('>H', int(value)))
+                else:
+                    f_out.write(struct.pack('>H', int(value)))
+        # low gain
+        for pixel in range(0, 7):
+            pedestal_value = (PedList[nr].meanped[1, pixel, :])
+            for value in (pedestal_value):
+                if np.isnan(value):
+                    value = 0
+                    f_out.write(struct.pack('>H', int(value)))
+                else:
+                    f_out.write(struct.pack('>H', int(value)))
+
+    f_out.close()
+
+
+path_to_data = os.path.join("/home", "pawel1", "Pulpit", "Astrophysics", "CTA", "dragon",
+                            "data", "dane_lst", "20180705", "Date20180705.0000.fits.fz")
+reader = LSTEventSource(input_url=path_to_data)
+seeker = EventSeeker(reader)
+ev = seeker[0]
+
+number_modules = ev.lst.tel[0].svc.num_modules
+ped = DragonPedestal()
+PedList = []
+
+for i in range(0, number_modules):
+    print("nr: ", i)
+    reader = LSTEventSource(input_url=path_to_data)
+    seeker = EventSeeker(reader)
+    PedList.append(DragonPedestal())
+    for j in range(0, 5000):
+        ev = seeker[j]
+        PedList[i].fill_pedestal_event(ev, nr=i)
+    PedList[i].finalize_pedestal()
+
+write_pedestal_to_file(PedList, number_modules)
